4e82d73a99f1ee431f462cefcbeaf1d2910f52d1
nitudeka <iamnitudeka@gmail.com>
2024-04-01 01:35:55 +0530

    added window move remaps

--- lua/d32/remap.lua
+++ lua/d32/remap.lua
@@ -7,6 +7,11 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { silent = true })
+
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
@@ -14,3 +19,4 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
