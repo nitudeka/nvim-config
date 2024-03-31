c855b071c6c2be13886e9f94a9695b3bd73117b9
nitudeka <iamnitudeka@gmail.com>
2024-04-01 01:35:43 +0530

    added gitsigns and lazygit

--- init.lua
+++ init.lua
@@ -33,5 +33,24 @@ require("lazy").setup({
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
 	{'L3MON4D3/LuaSnip'},
-
+  {'lewis6991/gitsigns.nvim'},
+	{
+					"kdheepak/lazygit.nvim",
+					cmd = {
+									"LazyGit",
+									"LazyGitConfig",
+									"LazyGitCurrentFile",
+									"LazyGitFilter",
+									"LazyGitFilterCurrentFile",
+					},
+					-- optional for floating window border decoration
+					dependencies = {
+									"nvim-lua/plenary.nvim",
+					},
+					-- setting the keybinding for LazyGit with 'keys' is recommended in
+					-- order to load the plugin when the command is run for the first time
+					keys = {
+									{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+					}
+	},
 })
