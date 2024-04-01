7d60e8718fca5348c7fb6cd77c3d79e8b94becc0
nitudeka <iamnitudeka@gmail.com>
2024-04-01 20:16:17 +0530

    cleaned plugins

--- init.lua
+++ init.lua
@@ -17,41 +17,37 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 require("lazy").setup({
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{ 'rose-pine/neovim', name = 'rose-pine' },
-	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
-	{'ThePrimeagen/harpoon'},
-	'mbbill/undotree',
-	'tpope/vim-fugitive',
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+  {'ThePrimeagen/harpoon'},
+  {'mbbill/undotree'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
   {'lewis6991/gitsigns.nvim'},
-	{'tpope/vim-commentary'},
-	{
-					"kdheepak/lazygit.nvim",
-					cmd = {
-									"LazyGit",
-									"LazyGitConfig",
-									"LazyGitCurrentFile",
-									"LazyGitFilter",
-									"LazyGitFilterCurrentFile",
-					},
-					-- optional for floating window border decoration
-					dependencies = {
-									"nvim-lua/plenary.nvim",
-					},
-					-- setting the keybinding for LazyGit with 'keys' is recommended in
-					-- order to load the plugin when the command is run for the first time
-					keys = {
-									{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-					}
-	},
+  {'tpope/vim-commentary'},
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
 })
